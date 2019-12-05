2f5eaa02b925c405d0f0500cc3d5d755d802d0f7
Emanoid <56281174+Emanoid@users.noreply.github.com>
2019-12-05 02:36:48 -0500

    Update HashingTest.java

--- HashingTest.java
+++ HashingTest.java
@@ -8,34 +8,6 @@ import org.junit.jupiter.api.Test;
 
 class HashingTest {
 
-	@Test
-	void Dictionary() {
-		IDictionary<String, String> d = new Dict<String, String>();
-		assertEquals(d.isEmpty(), true);
-		d.insert("Marco", "Morazan");
-		d.insert("James", "Kirk");
-		d.insert("Kathryn", "Janeway");
-		d.insert("Marco", "Morazan");
-		d.insert("B'Elanna", "Torres");
-		
-		assertEquals(d.isEmpty(), false);
-		assertEquals(d.size().intValue(), 5);
-		assertEquals(d.findElement("Marco"), "Morazan");
-		assertEquals(d.findElement("B'Elanna"), "Torres");
-		assertEquals(d.findElement("Josie"), null);
-		d.delete("James");
-		assertEquals(d.findElement("James"), null);
-		assertEquals(d.size().intValue(), 4);
-		
-		Iterator<String> klooper = d.keys();
-		Iterator<String> elooper = d.elements();
-		String res = "";
-		
-		while(klooper.hasNext()) {
-			res = res + " "+ klooper.next() + " " + elooper.next();}
-		assertEquals(res, " Marco Morazan Kathryn Janeway Marco Morazan B'Elanna Torres");
-	}
-	
 	
 	@Test
 	void LinearProbing() {
